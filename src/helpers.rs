24945c3b5eded859aa44e79ef64b40a4a00dda88
Sonia Pignorel <396009+soniapi@users.noreply.github.com>
2025-10-07 17:18:11 -0700

    feat(helpers): add option type handling

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,6 +1,6 @@
 use calamine::DataType;
 use std::any::type_name;
-use std::io::{self, Write};
+use std::io::{self, Write, BufRead};
 use std::error::Error;
 use std::fmt;
 
@@ -15,7 +15,26 @@ pub fn print_type<T>(_: &T) {
     println!("Type is: {}", type_name::<T>());
 }
 
-pub fn inputs() -> (String, String) {
+fn inputs_option() -> Option<String> {
+    let stdin = io::stdin();
+    let mut o = String::new();
+
+    match stdin.lock().read_line(&mut o) {
+        Ok(0) => None,
+        Ok(_) =>  {
+           let trimmed_o = o.trim_end().to_owned(); 
+           if trimmed_o.is_empty() {
+                None
+            }
+            else {
+                Some(trimmed_o)
+            }
+        }
+        Err(_) => None, 
+    }
+}
+
+pub fn inputs() -> (String, String, Option<String>) {
     let mut stdout = io::stdout();
     let stdin = io::stdin();
 
@@ -33,7 +52,12 @@ pub fn inputs() -> (String, String) {
     let trimmed_t = t.trim().to_string();
     println!("Your input:{:?}", trimmed_t);
 
-    (trimmed_f, trimmed_t)
+    stdout.flush().expect("Failed to flush stdout");
+    println!("Enter your partition type (no partition press enter):");
+    let trimmed_p: Option<String> = inputs_option();
+    println!("Your input {:?}", trimmed_p);
+
+    (trimmed_f, trimmed_t, trimmed_p)
 }
 
 
@@ -49,3 +73,4 @@ pub fn errors() -> Result<(), Box<dyn Error>> {
     impl Error for BaseError {}
     Err(Box::new(BaseError(message.to_string())))
 }
+
